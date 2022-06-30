044990828789925951eef41d10b31b17edb8e58a
sam@23.199 <you@example.com>
2022-06-30 18:19:25 +0800

    cp init/out registry-list --- fix build
    go mod cp; tidy
    
    go mod tidy
    
    clear alive22
    
    flast 参数调整
    
    // RootCmd.AddCommand(ConfCmd) //add
    
    +README
    ---
    
    up TODO
    
    up readme: RUN, 简介
    
    UP README
    ---
    TLS
    
    fix
    
    up
    
    sort
    
    validate
    
    +**专注项**
    
    +ig
    
    go flag
    
    init conf/
    ---
    full cp parser.go
    
    cp cut conf
    ---
    
    notes import
    
    +HTTP
    
    ref use conf/
    ---
    resolveConfiguration2; +config2
    
    configuration "cn.dev.registry-list/conf"
    
    drop README
    
    drop .ig

---
+++ conf/configuration.go
@@ -0,0 +1,186 @@
+package conf
+
+import (
+	// "errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	// "strings"
+	"time"
+
+	// "os"
+	// "sort"
+	// // "strconv"
+	// "strings"
+	// "github.com/sirupsen/logrus"
+	// "gopkg.in/yaml.v2"
+)
+
+// Configuration is a versioned registry configuration, intended to be provided by a yaml file, and
+// optionally modified by environment variables.
+//
+// Note that yaml field names should never include _ characters, since this is the separator used
+// in environment variable names.
+type Configuration struct {
+	
+	// HTTP contains configuration parameters for the registry's http
+	// interface.
+	HTTP struct {
+		// Addr specifies the bind address for the registry instance.
+		Addr string `yaml:"addr,omitempty"`
+
+		// Net specifies the net portion of the bind address. A default empty value means tcp.
+		Net string `yaml:"net,omitempty"`
+
+		// Host specifies an externally-reachable address for the registry, as a fully
+		// qualified URL.
+		Host string `yaml:"host,omitempty"`
+
+		Prefix string `yaml:"prefix,omitempty"`
+
+		// Secret specifies the secret key which HMAC tokens are created with.
+		Secret string `yaml:"secret,omitempty"`
+
+		// RelativeURLs specifies that relative URLs should be returned in
+		// Location headers
+		RelativeURLs bool `yaml:"relativeurls,omitempty"`
+
+		// Amount of time to wait for connection to drain before shutting down when registry
+		// receives a stop signal
+		DrainTimeout time.Duration `yaml:"draintimeout,omitempty"`
+
+		// TLS instructs the http server to listen with a TLS configuration.
+		// This only support simple tls configuration with a cert and key.
+		// Mostly, this is useful for testing situations or simple deployments
+		// that require tls. If more complex configurations are required, use
+		// a proxy or make a proposal to add support here.
+		TLS struct {
+			// Certificate specifies the path to an x509 certificate file to
+			// be used for TLS.
+			Certificate string `yaml:"certificate,omitempty"`
+
+			// Key specifies the path to the x509 key file, which should
+			// contain the private portion for the file specified in
+			// Certificate.
+			Key string `yaml:"key,omitempty"`
+
+			// Specifies the CA certs for client authentication
+			// A file may contain multiple CA certificates encoded as PEM
+			ClientCAs []string `yaml:"clientcas,omitempty"`
+
+			// Specifies the lowest TLS version allowed
+			MinimumTLS string `yaml:"minimumtls,omitempty"`
+
+			// Specifies a list of cipher suites allowed
+			CipherSuites []string `yaml:"ciphersuites,omitempty"`
+
+			// LetsEncrypt is used to configuration setting up TLS through
+			// Let's Encrypt instead of manually specifying certificate and
+			// key. If a TLS certificate is specified, the Let's Encrypt
+			// section will not be used.
+			LetsEncrypt struct {
+				// CacheFile specifies cache file to use for lets encrypt
+				// certificates and keys.
+				CacheFile string `yaml:"cachefile,omitempty"`
+
+				// Email is the email to use during Let's Encrypt registration
+				Email string `yaml:"email,omitempty"`
+
+				// Hosts specifies the hosts which are allowed to obtain Let's
+				// Encrypt certificates.
+				Hosts []string `yaml:"hosts,omitempty"`
+			} `yaml:"letsencrypt,omitempty"`
+		} `yaml:"tls,omitempty"`
+
+		// Headers is a set of headers to include in HTTP responses. A common
+		// use case for this would be security headers such as
+		// Strict-Transport-Security. The map keys are the header names, and
+		// the values are the associated header payloads.
+		Headers http.Header `yaml:"headers,omitempty"`
+
+		// Debug configures the http debug interface, if specified. This can
+		// include services such as pprof, expvar and other data that should
+		// not be exposed externally. Left disabled by default.
+		Debug struct {
+			// Addr specifies the bind address for the debug server.
+			Addr string `yaml:"addr,omitempty"`
+			// Prometheus configures the Prometheus telemetry endpoint.
+			Prometheus struct {
+				Enabled bool   `yaml:"enabled,omitempty"`
+				Path    string `yaml:"path,omitempty"`
+			} `yaml:"prometheus,omitempty"`
+		} `yaml:"debug,omitempty"`
+
+		// HTTP2 configuration options
+		HTTP2 struct {
+			// Specifies whether the registry should disallow clients attempting
+			// to connect via http2. If set to true, only http/1.1 is supported.
+			Disabled bool `yaml:"disabled,omitempty"`
+		} `yaml:"http2,omitempty"`
+	} `yaml:"http,omitempty"`
+
+
+
+	List struct {
+		// Apis string `yaml:"apis,omitempty"` 
+		User string `yaml:"user,omitempty"`
+		Pass string `yaml:"pass,omitempty"`
+		Size bool   `yaml:"size,omitempty"`
+	} `yaml:"list,omitempty"`
+}
+
+// v0_1Configuration is a Version 0.1 Configuration struct
+// This is currently aliased to Configuration, as it is the current version
+type v0_1Configuration Configuration
+
+
+
+// Parse parses an input configuration yaml document into a Configuration struct
+// This should generally be capable of handling old configuration format versions
+//
+// Environment variables may be used to override configuration parameters other than version,
+// following the scheme below:
+// Configuration.Abc may be replaced by the value of REGISTRY_ABC,
+// Configuration.Abc.Xyz may be replaced by the value of REGISTRY_ABC_XYZ, and so forth
+func Parse(rd io.Reader) (*Configuration, error) {
+	in, err := ioutil.ReadAll(rd)
+	if err != nil {
+		return nil, err
+	}
+
+	p := NewParser("registry", []VersionedParseInfo{
+		{
+			Version: MajorMinorVersion(0, 1),
+			ParseAs: reflect.TypeOf(v0_1Configuration{}),
+			ConversionFunc: func(c interface{}) (interface{}, error) {
+				if v0_1, ok := c.(*v0_1Configuration); ok {
+					/* if v0_1.Log.Level == Loglevel("") {
+						if v0_1.Loglevel != Loglevel("") {
+							v0_1.Log.Level = v0_1.Loglevel
+						} else {
+							v0_1.Log.Level = Loglevel("info")
+						}
+					}
+					if v0_1.Loglevel != Loglevel("") {
+						v0_1.Loglevel = Loglevel("")
+					}
+					if v0_1.Storage.Type() == "" {
+						return nil, errors.New("no storage configuration provided")
+					} */
+					return (*Configuration)(v0_1), nil
+				}
+				return nil, fmt.Errorf("expected *v0_1Configuration, received %#v", c)
+			},
+		},
+	})
+
+	config := new(Configuration)
+	err = p.Parse(in, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
---
+++ registry/app_list.go
@@ -0,0 +1,233 @@
+package registry
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"crypto/tls"
+	"flag"
+	"encoding/json"
+	"html/template"
+	"sort"
+	"strings"
+	//"strconv"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+
+	// "github.com/distribution/distribution/v3/configuration"
+	configuration "cn.dev.registry-list/conf"
+)
+
+var (
+	//addr = flag.String("addr", ":8080", "ui address")
+	// apis = flag.String("api", "https://localhost:8143", "api address")
+	user = flag.String("user", "admin", "registry user")
+	pass = flag.String("pass", "admin123", "registry pass")
+	size = flag.Bool("size", true, "registry size") //or: req's param
+	//tout = flag.Duration("tout", time.Second, "api cache timeout")
+	apis, host string // host:port of api server
+)
+func parseArgs(config *configuration.Configuration) {
+	flag.Parse()
+	//apis= config.List.Apis 
+	// apis= "http://localhost:8143"
+
+	// TODO extend Config类?
+	/* *user= "admin" //config.List.User
+	*pass= "admin123" //config.List.Pass
+	*size= true //config.List.Size */
+	*user= config.List.User
+	*pass= config.List.Pass
+	*size= config.List.Size
+
+	//set apis
+	schema:= "http://"
+	if ""!=config.HTTP.TLS.Certificate {
+		schema= "https://"
+	}
+	addr:= config.HTTP.Addr
+	strs:= strings.Split(addr,":")
+	if ""==strs[0] {
+		strs[0]="127.0.0.1"
+	}
+	apis=schema+strs[0]+":"+strs[1] 
+	//fmt.Println("apis="+apis) //apis=http://127.0.0.1:8143
+	
+	api, err := url.Parse(apis)
+	// api, err := url.Parse(config.HTTP.Addr) //url_err
+	if err != nil {
+		panic(err)
+	}
+	host = api.Host
+}
+
+func getClient(config *configuration.Configuration) (*http.Client) {
+	client := &http.Client{}
+
+	//TODO: 判断registry的参数是否为https模式；
+	//https://blog.csdn.net/weixin_43064185/article/details/125300410
+	if ""!=config.HTTP.TLS.Certificate {
+		// 忽略 https 证书验证
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client = &http.Client{Transport: transport}
+	}
+	return client
+}
+
+func doGet(client *http.Client, uri string, config *configuration.Configuration)(*http.Response, error){
+	url := apis + uri
+	req, err := http.NewRequest("GET", url, nil)
+	req.SetBasicAuth(*user, *pass)//设置需要认证的username和password
+	if err != nil {
+		panic(err)
+	}
+
+	//处理返回结果
+	return client.Do(req)
+}
+func imageList(w http.ResponseWriter, r *http.Request, config *configuration.Configuration) {
+	// fmt.Println("===config.List.Apis:  "+config.List.Apis)
+	parseArgs(config)
+	// fmt.Println("==============host:  "+host+config.HTTP.Addr)
+
+	//生成client 参数为默认
+	client := getClient(config) //&http.Client{}
+	
+	//catalog获取仓库列表
+	uri:="/v2/_catalog"
+	rsp, err := doGet(client, uri, config)
+	if err != nil {
+		// panic(err)
+		w.Write([]byte("RequestError, uri:"+uri))
+		return
+	}
+	//fmt.Println("rsp.StatusCode "+rsp.StatusCode)
+	if 200!=rsp.StatusCode {
+		w.Write([]byte("RequestError, Status:"+rsp.Status)) //strconv.Itoa(rsp.StatusCode)
+		return
+	}
+	// TODO: check status
+	var catalog struct {
+		Repos []string `json:"repositories"`
+	}
+	if err := json.NewDecoder(rsp.Body).Decode(&catalog); err != nil {
+		return 
+	}
+	if err := rsp.Body.Close(); err != nil {
+		return 
+	}
+
+	//tags/list获取各img的标签列表
+	imgMap := make(map[string][]string, len(catalog.Repos))
+	for _, name := range catalog.Repos {
+		//生成client 参数为默认
+		//client := &http.Client{}
+		uri= "/v2/" + name + "/tags/list"
+		rsp, err := doGet(client, uri, config)
+		if err != nil {
+			w.Write([]byte("RequestError, uri:"+uri))
+			return
+		}
+
+		/* defer rsp.Body.Close()
+		body, err := ioutil.ReadAll(rsp.Body)
+		fmt.Printf("%s\n",body)	 */	
+
+		// TODO: check status
+		var tags struct {
+			Name string   `json:"name"`
+			Tags []string `json:"tags"`
+		}
+		if err := json.NewDecoder(rsp.Body).Decode(&tags); err != nil {
+			return 
+		}
+		imgMap[name] = tags.Tags
+		if err := rsp.Body.Close(); err != nil {
+			return 
+		}
+	}
+
+	//auth remote.Option
+	auth := remote.WithAuth(authn.FromConfig(authn.AuthConfig{
+		Username: *user,
+		Password: *pass,
+	}))
+	// https, skip_key_validate
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	trans := remote.WithTransport(transport)
+	//遍历各img所有tags的layer层信息（获取size）
+	detailTags := make([]string, 0, len(catalog.Repos))
+	for oneImg, tags := range imgMap {
+		for _, tag := range tags {
+			if true!=*size {
+				detailTags = append(detailTags, oneImg+":"+tag)
+			} else {
+				tagName, _ := name.NewTag(host+"/"+oneImg+":"+tag) //host > apis
+				//remote获取layer层信息: http or https
+				var img v1.Image
+				if ""!=config.HTTP.TLS.Certificate {
+					img, err = remote.Image(tagName, auth, trans) //https
+				} else {
+					img, err = remote.Image(tagName, auth)
+				}
+				if err != nil {
+					println("causing failure " + err.Error())
+					//return nil
+					detailTags = append(detailTags, oneImg+":"+tag+" (errSize)")
+				} else {
+					//digest, _ := img.Digest()
+					layers, _ := img.Layers()
+
+					var imgSize int64
+					for _, layer := range layers {
+						size, _ := layer.Size()
+						imgSize += size
+					}
+					detailTags = append(detailTags, oneImg+":"+tag+" ("+ByteCountSI(imgSize)+")")
+				}
+			}
+		}
+	}
+	sort.Strings(detailTags)
+
+	if err := htmlTPL.Execute(w, detailTags); err != nil {
+		println("causing failure " + err.Error())
+	}
+	return 
+}
+
+var htmlTPL = template.Must(template.New("").Parse(`<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Registry Listing</title>
+	</head>
+	<body>
+		<ul>
+		{{- range .}}
+			<li>{{.}}</li>
+		{{- else}}<li>No Repositories Found</li>{{end}}
+		</ul>
+	</body>
+</html>`))
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
