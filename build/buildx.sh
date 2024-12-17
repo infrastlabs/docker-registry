f6a2870af6777e5b76067d33f5286a0617324a3a
sam@host-barge-headless <you@example.com>
2024-12-17 15:01:12 +0800

    buildx up

--- build/buildx.sh
+++ build/buildx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cur=$(cd "$(dirname "$0")"; pwd)
+cd $cur/..
 
 # ref _ct\fk-agent\_build.sh
 buidImg=registry.cn-shenzhen.aliyuncs.com/infrastlabs/golang:1.16.8-alpine3.14 #ref: dvp-ci-mgr>> maven:3.6.3-jdk-8-slim
@@ -12,7 +13,10 @@ function buildRegistry(){
   # ENV GO111MODULE=on
   # ENV GOPROXY=https://goproxy.cn
   # GOPATH: -v to cache
-  docker run -v $cur:/src \
+  test -d /mnt/mnt/data && pref=/mnt/data/
+  cur2=$(echo $(pwd)|sed "s/_ext/dbox_ext/g") #barge253
+  echo "==src: $pref$cur2"
+  docker run -v $pref$cur2:/src \
     -e GO111MODULE=on -e GOPROXY=https://goproxy.cn \
     $buidImg \
     sh -c "cd /src && ls -lh && sh build/go-build.sh; ls -lh build/*.tar.gz"
