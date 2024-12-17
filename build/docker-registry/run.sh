20195b3d7bc7f630a1cea2056e932edbb86dc5d2
sam@host-barge-headless <you@example.com>
2024-12-17 16:06:21 +0800

    build/registry/run.sh test -z "$REGISTRY_AUTH_HTPASSWD_PATH"

--- build/docker-registry/run.sh
+++ build/docker-registry/run.sh
@@ -4,15 +4,15 @@ cur=$(dirname $(readlink -f "$0"))
 
 # AUTH
 # export REGISTRY_AUTH=htpasswd
-export REGISTRY_AUTH_HTPASSWD_PATH=$cur/htpasswd.txt
-export REGISTRY_AUTH_HTPASSWD_REALM="basic-realm"
-export REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=$cur/data/
+test -z "$REGISTRY_AUTH_HTPASSWD_PATH" && export REGISTRY_AUTH_HTPASSWD_PATH=$cur/htpasswd.txt
+test -z "$REGISTRY_AUTH_HTPASSWD_REALM" && export REGISTRY_AUTH_HTPASSWD_REALM="basic-realm"
+test -z "$REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY" && export REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY=$cur/data/
 
 # HTTPS/HTTP
 # export REGISTRY_HTTP_ADDR=:8143 #改绑定端口(默认5000); 0.0.0.0:8143 
 domain=registry.local #test.registry.ssl
-export REGISTRY_HTTP_TLS_CERTIFICATE=$cur/certs/$domain.crt
-export REGISTRY_HTTP_TLS_KEY=$cur/certs/$domain.key 
+test -z "$REGISTRY_HTTP_TLS_CERTIFICATE" && export REGISTRY_HTTP_TLS_CERTIFICATE=$cur/certs/$domain.crt
+test -z "$REGISTRY_HTTP_TLS_KEY" && export REGISTRY_HTTP_TLS_KEY=$cur/certs/$domain.key 
 
 # EXTEND.HTTP,LIST
 # export REGISTRY_EXTEND_HTTPADDR=:9000 #REGISTRY_HTTP_ADDR开启tls模式时，额外开启http端口, ":0"则不开启
